48300cc2dc49754193f3a3bf9815c6ead28c259d
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2023-05-23 23:19:22 +0530

    gradious first commit\n

---
+++ login/js/login.js
@@ -0,0 +1,11 @@
+function openPopup() {
+  let p = document.getElementById("login-pop-up");
+  // if(p.style.display === "none") {
+  p.style.display = "block";
+  // }
+}
+function closePopup() {
+  let p = document.getElementById("login-pop-up");
+  p.style.display = "none";
+}
+
---
+++ popUp/js/pop.js
@@ -0,0 +1,10 @@
+function openPopup() {
+  let p = document.getElementById("pop-box");
+  // if(p.style.display === "none") {
+  p.style.display = "block";
+  // }
+}
+function closePopup() {
+  let p = document.getElementById("pop-box");
+  p.style.display = "none";
+}
